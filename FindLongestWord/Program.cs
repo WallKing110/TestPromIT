9cf58325ae7a966190e349355f99f3bbe5af3f49
WallKing110 <ksalamahin2004@mail.ru>
2025-07-27 17:24:34 +0500

    Сделал рабочую версию программы.

--- FindLongestWord/Program.cs
+++ FindLongestWord/Program.cs
@@ -1,7 +1,69 @@
-﻿class Program
-{
-    public void Main()
-    {
+﻿using System.Text;
 
+class Program
+{
+    static public void Main()
+    {
+        if (!File.Exists("text.txt"))
+        {
+            using (FileStream fs = new FileStream("text.txt", FileMode.OpenOrCreate))
+            {
+                string fillText = "Fill this file with your line.";
+                byte[] fillBuffer = Encoding.Default.GetBytes(fillText);
+                fs.Write(fillBuffer, 0, fillBuffer.Length);
+                Console.WriteLine("Required file didn't exist. We created 'text.txt' for program.");
+                fs.Close();
+                return;
+            }
+        }
+
+        FileStream fstream = new FileStream("text.txt", FileMode.Open, FileAccess.Read);
+        StringBuilder sb = new StringBuilder();
+        byte[] buffer = new byte[1024];
+        int bytesRead = 0;
+        using (StreamReader sr = new StreamReader(fstream, Encoding.Default))
+        {
+            while ((bytesRead = fstream.Read(buffer, 0, buffer.Length)) > 0)
+            {
+                char[] chars = Encoding.Default.GetChars(buffer, 0, bytesRead);
+                sb.Append(chars, 0, chars.Length);
+            }
+        }
+        fstream.Close();
+        string text = sb.ToString();
+
+        if (text.Length == 0)
+        {
+            Console.WriteLine("File is empty.");
+            return;
+        }
+
+        Func<char, bool> checkChar = c =>
+        {
+            return (!char.IsLetter(c) && !char.IsSymbol(' ')) || char.IsDigit(c);
+        };
+        string[] arr = text.Split(' ');
+        string maxStr = "";
+        for (int i = 0; i < arr.Length; ++i)
+        {
+            if (arr[i].Any(c => checkChar(c)))
+            {
+                Console.WriteLine("String \"" + arr[i] + "\" contains unallowed character.");
+                continue;
+            }
+            if (arr[i].Length > maxStr.Length)
+            {
+                maxStr = arr[i];
+            }
+        }
+
+        if (maxStr.Length == 0)
+        {
+            Console.WriteLine("Text doesn't have valid words.");
+        }
+        else
+        {
+            Console.WriteLine(maxStr);
+        }
     }
 }
